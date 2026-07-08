8d2d47b351ebae8d51bdeb230852e475b9ff1527
Pruthviraj Kalashetty <109294152+pruthviraj7171@users.noreply.github.com>
2026-07-08 15:48:15 +0530

    Refactor full subtractor to use half subtractor module

--- 03-Combinational-Logic/03-Adder-Subctractor.md/Rtl/full_subctractor_using_two_half_subctractor.v
+++ 03-Combinational-Logic/03-Adder-Subctractor.md/Rtl/full_subctractor_using_two_half_subctractor.v
@@ -1,37 +1,25 @@
 // 1) Code Full Subctractor Using Two Half Subctractor
 
 // 1) Half Subctractor
-module half_subtractor(
+module half_subctractor(
     input a, b,
-    output diff, borrow
+    output diffe, borrow
 );
-    assign diff   = a ^ b;      
+    assign diffe   = a ^ b;      
     assign borrow = ~a & b;     
 endmodule  
 
-// 2) Code Full Subtractor Using Half Subtractor
+// 2) Code Full Subctractor Using Half Subctractor
 module full_subtractor(
     input a, b, bin,            
-    output diff, borrow        
+    output diffe, borrow        
 );
     wire w1, w2, w3;
-  
-    // First Half Subtractor
-    half_subtractor h1 (
-        .a(a), 
-        .b(b), 
-        .diff(w1), 
-        .borrow(w2)
-    );
-    
-    // Second Half Subtractor
-    half_subtractor h2 (
-        .a(w1), 
-        .b(bin), 
-        .diff(diff),            
-        .borrow(w3)
-    );
-  
-    or o1(borrow, w2, w3);
 
-endmodule
+ //Two HAlf subctractor
+  half_subctractor h1(.a(a), .b(b), .diffe(w1), .borrow(w2));
+  half_subctractor h2 (.a(w1), .b(bin), .diffe(diffe), .borrow(w3));
+  
+    or o1(borrow,w2,w3);
+  endmodule
+   
