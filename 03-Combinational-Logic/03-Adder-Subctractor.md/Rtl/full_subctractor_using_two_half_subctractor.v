25b1f75a3d0b70912b423f471705ddc9aed104b2
Pruthviraj Kalashetty <109294152+pruthviraj7171@users.noreply.github.com>
2026-07-08 15:34:52 +0530

    Add full subtractor implementation using half subtractors

---
+++ 03-Combinational-Logic/03-Adder-Subctractor.md/Rtl/full_subctractor_using_two_half_subctractor.v
@@ -0,0 +1,35 @@
+// 1) Code Half Subtractor
+module half_subtractor(
+    input a, b,
+    output diff, borrow
+);
+    assign diff   = a ^ b;      
+    assign borrow = ~a & b;     
+endmodule  
+
+// 2) Code Full Subtractor Using Half Subtractor
+module full_subtractor(
+    input a, b, bin,            
+    output diff, borrow        
+);
+    wire w1, w2, w3;
+  
+    // First Half Subtractor
+    half_subtractor h1 (
+        .a(a), 
+        .b(b), 
+        .diff(w1), 
+        .borrow(w2)
+    );
+    
+    // Second Half Subtractor
+    half_subtractor h2 (
+        .a(w1), 
+        .b(bin), 
+        .diff(diff),            
+        .borrow(w3)
+    );
+  
+    or o1(borrow, w2, w3);
+
+endmodule
